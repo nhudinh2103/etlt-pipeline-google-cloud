4d2f6bd3ca3e2cd8146d938172d795d62f629370
dinhnn <nhudinh2103@gmail.com>
2025-02-06 00:29:33 +0700

    Update d_time.sql: change to create table if not exists (prevent create table multiple time)

--- dags/sql/d_time.sql
+++ dags/sql/d_time.sql
@@ -1,4 +1,4 @@
-CREATE OR REPLACE TABLE `personal-project-447516.airr_labs_interview.d_time` AS
+CREATE TABLE IF NOT EXISTS `personal-project-447516.airr_labs_interview.d_time` AS
 SELECT hour_value as d_time_id, FORMAT('%02d-%02d', 
     IF(hour_value = 0, 22, CAST(((hour_value - 1) / 3) * 3 + 1 AS INT64)),
     IF(hour_value = 0, 0, CAST(((hour_value - 1) / 3) * 3 + 3 AS INT64))
