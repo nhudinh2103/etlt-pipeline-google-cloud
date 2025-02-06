acd25fe122a05dd8b775f3c4a0c3e3190ad19668
dinhnn <nhudinh2103@gmail.com>
2025-02-06 23:52:40 +0700

    Remove alter table add primary key in init_table.sql

--- sql/init_table.sql
+++ sql/init_table.sql
@@ -27,7 +27,6 @@ SELECT hour_value as d_time_id, FORMAT('%02d-%02d',
     IF(hour_value = 0, 0, CAST(((hour_value - 1) / 3) * 3 + 3 AS INT64))
   ) AS hour_range_str
 FROM UNNEST(GENERATE_ARRAY(0, 23)) AS hour_value;
-ALTER TABLE  `personal-project-447516.airr_labs_interview.d_time` ADD PRIMARY KEY (d_time_id) NOT ENFORCED;
 
 CREATE TABLE IF NOT EXISTS `personal-project-447516.airr_labs_interview.f_commits_hourly`
 (
