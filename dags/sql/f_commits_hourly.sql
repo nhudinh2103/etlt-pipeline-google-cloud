3f710da83db09b8f95c4443c0c16736b63d404d6
dinhnn <nhudinh2103@gmail.com>
2025-02-06 13:55:27 +0700

    Fix syntax

--- dags/sql/f_commits_hourly.sql
+++ dags/sql/f_commits_hourly.sql
@@ -20,7 +20,7 @@ WHEN MATCHED THEN
 
 WHEN NOT MATCHED BY TARGET THEN
   INSERT (committer_id, committer_email, d_date_id, d_time_id, dt, commit_count)
-  VALUES (source.committer_id, source.committer_email, source.d_date_id, , source.d_time_id, source.dt, source.commit_count)
+  VALUES (source.committer_id, source.committer_email, source.d_date_id, source.d_time_id, source.dt, source.commit_count)
 
 WHEN NOT MATCHED BY SOURCE AND target.dt = '{{ ds }}' THEN
   UPDATE SET target.commit_count = 0;
